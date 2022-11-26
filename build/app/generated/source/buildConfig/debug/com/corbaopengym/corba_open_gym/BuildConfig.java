33b5615515f4fb911173ee746ecbc146a63fa40d
mathew <mathewbravo94@gmail.com>
2022-11-26 13:18:58 -0500

    Test

---
+++ build/app/generated/source/buildConfig/debug/com/corbaopengym/corba_open_gym/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.corbaopengym.corba_open_gym;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.corbaopengym.corba_open_gym";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
